51cd67949cd8e8f22fbd0f949a7308f96d62fa8f
v1rtl <pilll.pl22@gmail.com>
2021-08-11 11:41:53 +0300

    bump deps

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 // deno-lint-ignore-file
-import { ServerRequest } from 'https://deno.land/std@0.103.0/http/server.ts'
+import { ServerRequest } from 'https://deno.land/std@0.104.0/http/server.ts'
 import { default as ipaddr, IPv4, IPv6 } from 'https://cdn.skypack.dev/ipaddr.js'
-import { forwarded } from 'https://deno.land/x/forwarded@0.0.10/mod.ts'
+import { forwarded } from 'https://deno.land/x/forwarded@0.0.11/mod.ts'
 
 const DIGIT_REGEXP = /^[0-9]+$/
 const isip = ipaddr.isValid
